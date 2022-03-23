c7316012fb26ec8b8c771294a4fcab8b467b99bf
Alice Vigo <vigo.alice@gmail.com>
2022-03-23 13:31:22 +0100

    .

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+$(() => {
+    $('#lang').on('click', function() {
+        $('#ita').addClass('myHidden');
+        $('#eng').removeClass('myHidden');
+        $('#lang').addClass('myHidden');
+        $('#langIta').removeClass('myHidden');
+    });
+    $('#langIta').on('click', function() {
+        $('#eng').addClass('myHidden');
+        $('#ita').removeClass('myHidden');
+        $('#langIta').addClass('myHidden');
+        $('#lang').removeClass('myHidden');
+    });
+})
